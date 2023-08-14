f057df709f5cd8679edfd4d3884beb75e6e13523
SelçukBaşak <selcukbasak5@gmail.com>
2023-08-15 00:58:03 +0300

    Update Apollo.js
    test

--- src/chat/Apollo.js
+++ src/chat/Apollo.js
@@ -4,7 +4,7 @@ import { getMainDefinition } from "@apollo/client/utilities";
 import { WebSocketLink } from "@apollo/client/link/ws";
 
 const admin = process.env.REACT_APP_API_KEY;
-console.log(admin);
+
 
 const httpLink = new HttpLink({
   uri: "https://sefihuom-chat-server.hasura.app/v1/graphql",
@@ -44,18 +44,6 @@ const client = new ApolloClient({
   cache: new InMemoryCache(),
 });
 
-const socket = new WebSocket("ws://sefihuom-chat-server.hasura.app/v1/graphql");
 
-socket.addEventListener("open", (event) => {
-  console.log("WebSocket connection opened:", event);
-});
-
-socket.addEventListener("message", (event) => {
-  console.log("WebSocket message received:", event.data);
-});
-
-socket.addEventListener("close", (event) => {
-  console.log("WebSocket connection closed:", event);
-});
 
 export default client;
