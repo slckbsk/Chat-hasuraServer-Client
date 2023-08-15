7ef5ed48cd4c17d84e09080491479d7f565d0d6d
SelçukBaşak <selcukbasak5@gmail.com>
2023-08-16 02:10:17 +0300

    Update UsersList.js

--- src/chat/UsersList.js
+++ src/chat/UsersList.js
@@ -3,22 +3,21 @@ import { useQuery, useSubscription } from "@apollo/client";
 import { GET_USERS, USERS_COUNT } from "./queries";
 import ListGroup from "react-bootstrap/ListGroup";
 import Loading from "./Loading";
+import Form from "react-bootstrap/Form";
 
 const UserSubscription = () => {
   const { loading, error, data, refetch } = useQuery(GET_USERS);
   const { data: subscriptionData } = useSubscription(USERS_COUNT);
 
+  const userCount = subscriptionData?.users_aggregate?.aggregate?.count;
+
   useEffect(() => {
-    if (
-      subscriptionData &&
-      data &&
-      subscriptionData.count !== data.users.length
-    ) {
+    if (subscriptionData && data && userCount !== data.users.length) {
       refetch();
     }
-  }, [subscriptionData, data, refetch]);
+  }, [subscriptionData, data, userCount, refetch]);
 
-  if (loading) return <Loading/>;
+  if (loading) return <Loading />;
   if (error) return <div>Error!: {error.message}</div>;
 
   const newUser = data.users;
@@ -31,6 +30,7 @@ const UserSubscription = () => {
           overflowY: "scroll",
         }}
       >
+        <Form.Label>ONLINE USER LIST / {userCount} PEOPLE ONLINE</Form.Label>
         {newUser.map((user, index) => (
           <div key={index}>
             <ListGroup.Item>{user.user}</ListGroup.Item>
